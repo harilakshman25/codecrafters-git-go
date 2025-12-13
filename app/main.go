be0876e4e8ea7025be9fbec49140067c492a7f21
harilakshman25 <harilakshman509716@kgpian.iitkgp.ac.in>
2025-12-13 11:07:26 +0000

    Read Blob

--- app/main.go
+++ app/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"compress/zlib"
+	"errors"
 	"fmt"
 	"os"
+	"strings"
+	"path"
+	"io"
 )
 
 // Usage: your_program.sh <command> <arg1> <arg2> ...
@@ -17,21 +23,90 @@ func main() {
 
 	switch command := os.Args[1]; command {
 	case "init":
-		for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
-			if err := os.MkdirAll(dir, 0755); err != nil {
-				fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
-			}
-		}
-		
-		headFileContents := []byte("ref: refs/heads/main\n")
-		if err := os.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
-			fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
-		}
-		
+		initRepo("")
 		fmt.Println("Initialized git directory")
+	case "cat-file":
+		if len(os.Args) < 4 {
+			handleError(errors.New("usage: mygit cat-file -p [<args>...]"))
+			os.Exit(1)
+		}
+
+		if os.Args[2] != "-p" {
+           handleError(errors.New("usage: mygit cat-file -p [<args>...]"))
+		   os.Exit(1)
+		}
+
+		content, err := readContentObject(os.Args[3])
+		if err != nil {
+			handleError(err)
+			return
+		}
+
+		fmt.Print(content)
 
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 		os.Exit(1)
 	}
+}
+
+func initRepo(repoPath string) {
+	for _, dir := range []string{".git", ".git/objects", ".git/refs", ".git/refs/heads"} {
+		dirPath := path.Join(repoPath, dir)
+		if err := os.MkdirAll(dirPath, 0755); err != nil {
+			fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
+		}
+	}
+
+	headFileContents := []byte("ref: refs/heads/master\n")
+	if err := os.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
+	}
+}
+
+func readContentObject(hash string) (string, error) {
+	if len(hash) != 40 {
+		return "", fmt.Errorf("invalid len of the hash")
+	}
+
+	buf := readObject(hash)
+	parts := strings.SplitN(buf.String(), "\x00", 2)
+	if len(parts) != 1 {
+       return "", fmt.Errorf("invalid object")
+	}
+	return parts[0], nil
+}
+
+func readObject(hash string) bytes.Buffer {
+    dir := fmt.Sprintf(".git/objects/%s", hash[:2])
+	fileName := fmt.Sprintf("%s/%s", dir, hash[2:])
+
+	fileContent, err := os.ReadFile(fileName) 
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "read file got err=%v", err)
+		os.Exit(1)
+	}
+
+    rcloser, err := zlib.NewReader(bytes.NewReader(fileContent))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "zlib new reader got err=%v", err)
+		os.Exit(1)
+	}
+
+	defer rcloser.Close()
+
+	var buf bytes.Buffer
+	if _, err := io.Copy(&buf, rcloser); err != nil {
+		fmt.Fprintf(os.Stderr, "read file to buffer got err=%v\n", err)
+		os.Exit(1)
+	}
+
+	return buf
+
+}
+
+
+
+func handleError(err error) {
+	fmt.Fprint(os.Stderr, err.Error()+"\n")
 }
